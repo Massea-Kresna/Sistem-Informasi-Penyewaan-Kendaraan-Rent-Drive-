a25a4f94d4e139538647e99681f01f8b2c80c654
Joshpeter14 <132868467+Joshpeter14@users.noreply.github.com>
2026-05-06 20:14:57 +0700

    Update Penyewaan.php (#3)

--- app/Models/Penyewaan.php
+++ app/Models/Penyewaan.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Penyewaan extends Model
 {
-    //
+    protected $table = 'penyewaan';
+    protected $primaryKey = 'id_sewa';
+    protected $guarded = [];
+    public $timestamps = false;
 }
